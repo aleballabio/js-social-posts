ccca3df9497842d7e6dd4d68cc6a9c86829f412c
aleballabio <aleballabio05@gmail.com>
2022-03-08 17:39:38 +0100

    cannot select all likes button

--- main.js
+++ main.js
@@ -73,55 +73,58 @@ for (let times = 0; times < posts.length; times++) {
 }
 
 
-console.log(arrPostsID);
 
 //Posts Creation
 function GetPostCreation(obj) {
     let container = document.querySelector(".posts-list");
 
     let postCreation = `
-        <div class="post">
+    <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${obj.author.image}" alt="${obj.author.name}">
+                <div class="post-meta__icon">
+                <img class="profile-pic" src="${obj.author.image}" alt="${obj.author.name}">
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${obj.author.name}</div>
-                        <div class="post-meta__time">${obj.created}</div>
+                    <div class="post-meta__author">${obj.author.name}</div>
+                    <div class="post-meta__time">${obj.created}</div>
                     </div>
-                </div>
-            </div>
-            <div class="post__text">${obj.content}</div>
-            <div class="post__image">
+                    </div>
+                    </div>
+                <div class="post__text">${obj.content}</div>
+                <div class="post__image">
                 <img src="${obj.media}" alt="">
             </div>
             <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
+            <div class="likes js-likes">
+            <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="${obj.id}">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
                         </a>
-                    </div>
-                    <div class="likes__counter">
+                        </div>
+                        <div class="likes__counter">
                         Piace a <b id="like-counter-1" class="js-likes-counter">${obj.likes}</b> persone
-                    </div>
-                </div>
-            </div>
-        </div>`;
-    let btnLikes = document.querySelector(".like-button");
-    btnLikes.addEventListener("click", addLikes(event));
+                        </div>
+                        </div>
+                        </div>
+                        </div>`;
 
-    //Button Likes
+    container.innerHTML += postCreation;
+    postCreation = document.querySelector(".post")
+    let btnLikes = postCreation.querySelector(".js-like-button");
+    btnLikes.addEventListener("click", addLikes);
+
     function addLikes(event) {
         let counter = obj.likes;
         counter++;
+        arrPostsIDLiked.push(obj.id);
         btnLikes.classList.toggle("like-button--liked");
         event.preventDefault();
+        console.log(arrPostsIDLiked);
+
     }
 
-    container.innerHTML += postCreation;
 }
 
 
